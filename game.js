6e7016e83339cfc01f25515efb72edbd6ebcd338
Anton Kovalyov <thebyagent@gmail.com>
2014-09-24 21:31:10 +0300

    Basic design

--- game.js
+++ game.js
@@ -1,8 +1,22 @@
 (function() {
-  var i;
+  var makeMap, map, map_size;
 
-  i = 5;
+  map_size = 20;
 
-  alert("heloo");
+  map = [];
+
+  makeMap = function(size) {
+    var col, row, _i, _j, _results;
+    map = [];
+    _results = [];
+    for (row = _i = 0; 0 <= size ? _i <= size : _i >= size; row = 0 <= size ? ++_i : --_i) {
+      row = [];
+      for (col = _j = 0; 0 <= size ? _j <= size : _j >= size; col = 0 <= size ? ++_j : --_j) {
+        row.push(0);
+      }
+      _results.push(map.push(row));
+    }
+    return _results;
+  };
 
 }).call(this);
--- src/game.coffee
+++ src/game.coffee
@@ -1,2 +1,14 @@
-i = 5
-alert("heloo")
+map_size = 20
+
+map = []
+
+makeMap = (size) ->
+	map = []
+	for row in [0..size]
+		row = []
+		for col in [0..size]
+			row.push 0
+		map.push row
+
+#TODO: make render function
+
