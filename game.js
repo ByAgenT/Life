0e2e5fba619ce3738cc30f1777342913e9d18fe1
Anton Kovalyov <thebyagent@gmail.com>
2014-09-25 21:51:33 +0300

    Added jQuery events, map generation

--- game.js
+++ game.js
@@ -1,40 +1,53 @@
 (function() {
   var makeMap, map, map_size, renderMap;
 
-  map_size = 20;
+  map_size = 60;
 
   map = [];
 
   makeMap = function(size) {
-    var col, row, _i, _j, _results;
+    var col, row, _i, _j;
     map = [];
-    _results = [];
-    for (row = _i = 0; 0 <= size ? _i <= size : _i >= size; row = 0 <= size ? ++_i : --_i) {
+    for (row = _i = 1; 1 <= size ? _i <= size : _i >= size; row = 1 <= size ? ++_i : --_i) {
       row = [];
-      for (col = _j = 0; 0 <= size ? _j <= size : _j >= size; col = 0 <= size ? ++_j : --_j) {
+      for (col = _j = 1; 1 <= size ? _j <= size : _j >= size; col = 1 <= size ? ++_j : --_j) {
         row.push(0);
       }
-      _results.push(map.push(row));
+      map.push(row);
     }
-    return _results;
+    return map;
   };
 
   renderMap = function(map) {
-    var $map, cell, row, _i, _j, _len, _len1;
+    var $map, cell, id, row, _i, _j, _len, _len1;
     console.log("Start map render...");
+    id = 1;
     $map = $('#map').empty();
     for (_i = 0, _len = map.length; _i < _len; _i++) {
       row = map[_i];
       for (_j = 0, _len1 = row.length; _j < _len1; _j++) {
         cell = row[_j];
-        $map.append($("<div>").addClass("c-" + cell));
+        $map.append($("<div>").attr('id', id).addClass('dead'));
+        id++;
       }
     }
     return console.log("Map render finished!");
   };
 
   $(function() {
-    return renderMap(makeMap(map_size));
+    map = makeMap(map_size);
+    renderMap(map);
+    return $("#map div").click(function() {
+      if ($(this).hasClass('dead')) {
+        $(this).removeClass('dead').addClass('alive');
+        map[$(this).attr('id')] = 1;
+        return console.log("Changed to: " + map[$(this).attr('id')]);
+      } else {
+        $(this).removeClass('alive').addClass('dead');
+        map[$(this).attr('id')] = 0;
+        return console.log("Changed to: " + map[$(this).attr('id')]);
+      }
+    });
   });
 
 }).call(this);
--- src/game.coffee
+++ src/game.coffee
@@ -1,22 +1,38 @@
-map_size = 20
-
+map_size = 60
 map = []
 
 makeMap = (size) ->
-	map = []
-	for row in [0..size]
-		row = []
-		for col in [0..size]
-			row.push 0
-		map.push row
+    map = []
+    for row in [1..size]
+        row = []
+        for col in [1..size]
+            row.push 0
+        map.push row
+    map
 
-#TODO: make render function
 renderMap = (map) ->
-	console.log "Start map render..."
-	$map = $('#map').empty()
-	for row in map
-		for cell in row
-			$map.append $("<div>").addClass("c-#{cell}")
-	console.log "Map render finished!"
+    console.log "Start map render..."
+    id = 1
+    $map = $('#map').empty()
+    for row in map
+        for cell in row
+            $map.append $("<div>").attr('id', id).addClass('dead')
+            id++
+    console.log "Map render finished!"
+
 $ ->
-	renderMap makeMap(map_size)
+    map = makeMap(map_size)
+    renderMap map
+    $("#map div").click ->
+        if $(@).hasClass('dead')
+            $(@).removeClass('dead').addClass('alive')
+            map[$(@).attr('id')] = 1
+            console.log("Changed to: " + map[$(@).attr('id')])
+        else 
+            $(@).removeClass('alive').addClass('dead')
+            map[$(@).attr('id')] = 0
+            console.log("Changed to: " + map[$(@).attr('id')])
+
+
+
+
