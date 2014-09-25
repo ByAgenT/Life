7a44b941fc5a91e3a165a3d064da07d0579f5070
Anton Kovalyov <thebyagent@gmail.com>
2014-09-26 01:48:56 +0300

    Coordinates

--- game.js
+++ game.js
@@ -1,17 +1,39 @@
 (function() {
-  var makeMap, map, map_size, renderMap;
+  var Cell, getNeighbours, makeMap, map, map_size, renderMap;
 
   map_size = 60;
 
   map = [];
 
+  Cell = (function() {
+    function Cell(isAlive, id, x, y) {
+      this.isAlive = isAlive;
+      this.id = id;
+      this.x = x;
+      this.y = y;
+    }
+
+    Cell.prototype.isAlive = false;
+
+    Cell.prototype.id = 0;
+
+    Cell.prototype.x = 0;
+
+    Cell.prototype.y = 0;
+
+    return Cell;
+
+  })();
+
   makeMap = function(size) {
-    var col, row, _i, _j;
+    var col, id, row, rows, _i, _j;
     map = [];
-    for (row = _i = 1; 1 <= size ? _i <= size : _i >= size; row = 1 <= size ? ++_i : --_i) {
+    id = 1;
+    for (rows = _i = 1; 1 <= size ? _i <= size : _i >= size; rows = 1 <= size ? ++_i : --_i) {
       row = [];
       for (col = _j = 1; 1 <= size ? _j <= size : _j >= size; col = 1 <= size ? ++_j : --_j) {
-        row.push(0);
+        row.push(new Cell(false, id, rows, col));
+        id++;
       }
       map.push(row);
     }
@@ -19,33 +41,71 @@
   };
 
   renderMap = function(map) {
-    var $map, cell, id, row, _i, _j, _len, _len1;
+    var $map, cell, row, _i, _j, _len, _len1;
     console.log("Start map render...");
-    id = 1;
     $map = $('#map').empty();
     for (_i = 0, _len = map.length; _i < _len; _i++) {
       row = map[_i];
       for (_j = 0, _len1 = row.length; _j < _len1; _j++) {
         cell = row[_j];
-        $map.append($("<div>").attr('id', id).addClass('dead'));
-        id++;
+        if (cell.isAlive) {
+          $map.append($("<div>").attr('id', cell.id).addClass('alive'));
+        } else {
+          $map.append($("<div>").attr('id', cell.id).addClass('dead'));
+        }
       }
     }
     return console.log("Map render finished!");
   };
 
+  getNeighbours = function(map, x, y) {
+    var count;
+    count = 0;
+    if (map[x + 1][y].isAlive) {
+      count++;
+    }
+    if (map[x + 1][y + 1].isAlive) {
+      count++;
+    }
+    if (map[x][y + 1].isAlive) {
+      count++;
+    }
+    if (map[x - 1][y + 1].isAlive) {
+      count++;
+    }
+    if (map[x - 1][y].isAlive) {
+      count++;
+    }
+    if (map[x - 1][y - 1].isAlive) {
+      count++;
+    }
+    if (map[x][y - 1].isAlive) {
+      count++;
+    }
+    if (map[x + 1][y - 1].isAlive) {
+      count++;
+    }
+    return count;
+  };
+
   $(function() {
     map = makeMap(map_size);
+    console.log(map);
     renderMap(map);
     return $("#map div").click(function() {
+      var id, x, y;
+      id = $(this).attr('id');
+      x = Math.floor(id / 61) + 1;
+      y = id % 60 || 60;
+      console.log("x: " + x + " y: " + y);
       if ($(this).hasClass('dead')) {
         $(this).removeClass('dead').addClass('alive');
-        map[$(this).attr('id')] = 1;
-        return console.log("Changed to: " + map[$(this).attr('id')]);
+        map[x - 1][y - 1].isAlive = true;
+        return console.log("Neighbours: " + getNeighbours(map, x - 1, y - 1));
       } else {
         $(this).removeClass('alive').addClass('dead');
-        map[$(this).attr('id')] = 0;
-        return console.log("Changed to: " + map[$(this).attr('id')]);
+        map[x - 1][y - 1].isAlive = false;
+        return console.log("Changed to: " + map[x - 1][y - 1]);
       }
     });
   });
--- src/game.coffee
+++ src/game.coffee
@@ -1,37 +1,70 @@
 map_size = 60
 map = []
 
+class Cell
+    constructor: (isAlive, id, x, y) ->
+        @.isAlive = isAlive
+        @.id = id
+        @.x = x
+        @.y = y
+    isAlive: false
+    id: 0
+    x: 0
+    y: 0
+
+#Map Generating
 makeMap = (size) ->
     map = []
-    for row in [1..size]
+    id = 1
+    for rows in [1..size]
         row = []
         for col in [1..size]
-            row.push 0
+            row.push(new Cell(false, id, rows, col))
+            id++
         map.push row
     map
 
+#Render map to page
 renderMap = (map) ->
     console.log "Start map render..."
-    id = 1
     $map = $('#map').empty()
     for row in map
         for cell in row
-            $map.append $("<div>").attr('id', id).addClass('dead')
-            id++
+            if cell.isAlive then $map.append $("<div>").attr('id', cell.id).addClass('alive') else $map.append $("<div>").attr('id', cell.id).addClass('dead')          
     console.log "Map render finished!"
+
+#Get neighbours alive cells
+getNeighbours = (map, x, y) ->
+    count = 0
+    count++ if map[x+1][y].isAlive
+    count++ if map[x+1][y+1].isAlive
+    count++ if map[x][y+1].isAlive
+    count++ if map[x-1][y+1].isAlive
+    count++ if map[x-1][y].isAlive
+    count++ if map[x-1][y-1].isAlive
+    count++ if map[x][y-1].isAlive
+    count++ if map[x+1][y-1].isAlive
+    count
+
 
 $ ->
     map = makeMap(map_size)
+    console.log map
     renderMap map
     $("#map div").click ->
+        id = $(@).attr('id')
+        x = Math.floor(id / 61) + 1
+        y = id % 60 || 60
+        console.log "x: " + x + " y: " + y
         if $(@).hasClass('dead')
             $(@).removeClass('dead').addClass('alive')
-            map[$(@).attr('id')] = 1
-            console.log("Changed to: " + map[$(@).attr('id')])
+            map[x-1][y-1].isAlive = true
+            #TODO: check getNeighbours
+            console.log "Neighbours: " + getNeighbours(map, x-1, y-1)
         else 
             $(@).removeClass('alive').addClass('dead')
-            map[$(@).attr('id')] = 0
-            console.log("Changed to: " + map[$(@).attr('id')])
+            map[x-1][y-1].isAlive = false
+            console.log("Changed to: " + map[x-1][y-1])
 
 
 
